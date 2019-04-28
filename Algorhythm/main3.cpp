eee07b9f221c9debcb4fa95dfcf732171873d18d
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2019-04-28 17:05:53 +0900

    演算子のオーバーライドを実装

--- Algorhythm/main3.cpp
+++ Algorhythm/main3.cpp
@@ -9,8 +9,10 @@ class Point2d
 public:
     Point2d();
     Point2d(float x, float y);
-    float GetX();
-    float GetY();
+    float GetX() const;
+    float GetY() const;
+    Point2d operator + (Point2d obj);
+    Point2d operator * (float val);
 private:
     float m_X;
     float m_Y;
@@ -27,14 +29,30 @@ Point2d::Point2d(float x, float y)
     m_Y = y;
 }
 
-float Point2d::GetX()
+float Point2d::GetX() const
 {
     return m_X;
 }
 
-float Point2d::GetY()
+float Point2d::GetY() const
 {
     return m_Y;
+}
+
+Point2d Point2d::operator+(Point2d obj)
+{
+    float newX = m_X + obj.GetX();
+    float newY = m_Y + obj.GetY();
+    Point2d newP(newX, newY);
+    return newP;
+}
+
+Point2d Point2d::operator*(float val)
+{
+    float newX = m_X * val;
+    float newY = m_Y * val;
+    Point2d newP(newX, newY);
+    return newP;
 }
 
 class Vector2d
@@ -45,6 +63,9 @@ public:
     Vector2d GetRotate(float degree);       // 始点から時計回りにtheta回転させたベクトルを取得
     Point2d GetPointBegin();
     Point2d GetPointEnd();
+    Vector2d operator + (Vector2d obj);
+    Vector2d operator * (float val);
+
 private:
     Point2d m_Begin;
     Point2d m_End;
@@ -79,6 +100,27 @@ Point2d Vector2d::GetPointBegin()
 Point2d Vector2d::GetPointEnd()
 {
     return m_End;
+}
+
+Vector2d Vector2d::operator+(Vector2d obj)
+{
+    float newX = m_End.GetX() + obj.m_End.GetX();
+    float newY = m_End.GetY() + obj.m_End.GetY();
+    Vector2d newV(m_Begin, Point2d(newX, newY));
+    return newV;
+}
+
+Vector2d Vector2d::operator*(float val)
+{
+    float newX = m_Begin.GetX() + (m_End.GetX() - m_Begin.GetX()) * val;
+    float newY = m_Begin.GetY() + (m_End.GetY() - m_Begin.GetY()) * val;
+    Vector2d newV(m_Begin, Point2d(newX, newY));
+    return newV;
+}
+
+void koch(int n, const Point2d* p1, const Point2d* p2)
+{
+    
 }
 
 
