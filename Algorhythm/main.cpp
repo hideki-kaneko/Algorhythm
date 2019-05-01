f8d5bf9919bc00d76f9b4e521837c1e5d6a2acff
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2019-05-01 16:14:03 +0900

    全プログラムが実行される不具合を修正

--- Algorhythm/main.cpp
+++ Algorhythm/main.cpp
@@ -11,22 +11,31 @@ int main()
     {
     case 1:
         main1();
+        break;
     case 2:
         main2();
+        break;
     case 3:
         main3();
+        break;
     case 4:
         main4();
+        break;
     case 5:
         main5();
+        break;
     case 6:
         main6();
+        break;
     case 7:
         main7();
+        break;
     case 8:
         main8();
+        break;
     case 9:
         main9();
+        break;
     default:
         break;
     }
