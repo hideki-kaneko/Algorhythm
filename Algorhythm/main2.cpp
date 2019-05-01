69b854940efef258be0c72d599de1668ddb92cfb
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2019-05-01 16:07:30 +0900

    動的計画法による高速化を実装

--- Algorhythm/main2.cpp
+++ Algorhythm/main2.cpp
@@ -7,32 +7,54 @@ using namespace std;
 
 #define MAX_N 20
 #define MAX_Q 200
+#define MAX_M 2000
 
 namespace{
-int g_N; // 数列Ano長さ
+int g_N;               // 数列Aの長さ
+int g_Dp[MAX_Q][MAX_M];// 計算結果メモ用配列 <-1:未計算 0:false 1:true>
 }
 
-// 長さg_Nの数列Aの要素を足してmが作れる場合Trueを返します
+// 長さg_Nの数列Aの、i番目以降の要素を足してmが作れる場合Trueを返します
 bool Solve(const int* A, int i, int m)
 {
-    if (m == 0)
-    {
-        return true;
-    }
-    if (i >= g_N && m != 0)
+    if (m < 0)
     {
         return false;
     }
-    bool a1 = Solve(A, i + 1, m - A[i]);  // i番目の数を使う
-    bool a2 = Solve(A, i + 1, m);         // 使わない
-    return (a1 || a2);
+    else if (g_Dp[i][m] != -1)
+    {
+        return g_Dp[i][m]; // Solve(i, m)が計算済み
+    }
+    else if (m == 0)                          // mを作れた
+    {
+        g_Dp[i][m] = true;
+    }
+    else if (i >= g_N)                   // N個使ってもmを作れなかった
+    {
+        g_Dp[i][m] = false;
+    }
+    else
+    {
+        g_Dp[i][m] = Solve(A, i + 1, m - A[i]) || Solve(A, i + 1, m);
+    }
+
+    return g_Dp[i][m];
 }
 
 int main2()
 {
-    int A[MAX_N];  // 数列A
-    int q;         // 整数mの個数
-    int m[MAX_Q];  // q個の整数m
+    int A[MAX_N];        // 数列A
+    int q;               // 整数mの個数
+    int m[MAX_Q];        // q個の整数m
+    
+    
+    for (int i = 0; i < MAX_Q; i++)
+    {
+        for (int j = 0; j < MAX_M; j++)
+        {
+            g_Dp[i][j] = -1;
+        }
+    }
 
     // 入力
     cin >> g_N;
